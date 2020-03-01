f6c5f2f6b4070bcb64280e970b32d0c2b87e23ff
JohnWreck <john.wreck@gmail.com>
2020-03-01 18:57:02 +0100

    New install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ~
+
+mv .vimrc .vimrc.bak
+mv .i3blocks.conf .i3blocks.conf.bak
+mv .scripts/ .scripts.bak/
+mv .bashrc .bashrc.bak
+mv .icons/ .icons.bak/
+mv .fonts/ .fonts.bak/
+
+ln -s .dotfiles/.vimrc .vimrc
+ln -s .dotfiles/.i3blocks.conf .i3blocks.conf
+ln -s .dotfiles/.i3/ .i3
+ln -s .dotfiles/.scripts/ .scripts
+ln -s .dotfiles/.bashrc .bashrc
+ln -s .dotfiles/.icons/ .icons
+ln -s .dotfiles/.fonts/ .fonts
