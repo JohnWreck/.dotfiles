f84139165b1f1bb342bc1466b26b45829b3eccd0
JohnWreck <john.wreck@gmail.com>
2020-03-20 09:04:20 +0100

    i3 install

--- i3_install.sh
+++ i3_install.sh
@@ -1,6 +1,6 @@
 sudo apt update
 
-## i3 install
+## i3 install Ubuntu 18.04(netinstall)
 sudo apt install xorg
 sudo apt install i3 i3blocks pavucontrol lxappearance fonts-firacode breez-cursor-theme cmus feh vim htop compton gnome-terminal nautilus numlockx xbacklight \
 gir1.2-gtop-2.0 lm-sensors network-manager bc \
