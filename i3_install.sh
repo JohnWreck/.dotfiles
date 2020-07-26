f849db23947541c2b8951772390e073104beb2de
JohnWreck <john.wreck@gmail.com>
2020-07-26 10:24:55 +0200

    edit

--- i3_install.sh
+++ i3_install.sh
@@ -5,7 +5,7 @@ apt install xorg i3 i3blocks dkms software-properties-common curl
 add-apt-repository ppa:mmstick76/alacritty
 apt update
 
-apt install lxappearance mpv fonts-firacode maim cmus feh vim-gtk3 htop compton ranger alacritty numlockx xbacklight gir1.2-gtop-2.0 lm-sensors network-manager bc sxiv
+apt install lxappearance mpv fonts-firacode maim cmus feh vim-gtk3 htop compton ranger alacritty numlockx xbacklight gir1.2-gtop-2.0 lm-sensors network-manager bc sxiv flameshot
 
 
 ## Vim-Plug
