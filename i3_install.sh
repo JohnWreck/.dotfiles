33489d7b89b8c894a518c4658a8c01453934d6af
JohnWreck <john.wreck@gmail.com>
2020-08-15 15:50:14 +0200

    i3 edit

--- i3_install.sh
+++ i3_install.sh
@@ -1,11 +1,11 @@
 ## i3 install Ubuntu 18.04(netinstall)
 apt update
-apt install xorg i3 i3blocks dkms software-properties-common curl
+apt install xinit i3 i3blocks dkms software-properties-common curl
 
 add-apt-repository ppa:mmstick76/alacritty
 apt update
 
-apt install lxappearance mpv fonts-firacode cmus feh vim-gtk3 htop compton ranger alacritty numlockx xbacklight gir1.2-gtop-2.0 lm-sensors network-manager bc sxiv flameshot
+apt install rofi lxappearance mpv fonts-firacode cmus feh vim-gtk3 htop compton ranger alacritty numlockx xbacklight gir1.2-gtop-2.0 lm-sensors network-manager bc sxiv flameshot
 
 ## Vim-Plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
