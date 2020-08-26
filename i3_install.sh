2421f4aad93f9a13d33f96a38c0e3f06a8df5365
johnwreck <john.wreck@gmail.com>
2020-08-26 15:23:38 +0200

    nvim

--- i3_install.sh
+++ i3_install.sh
@@ -5,6 +5,6 @@ apt install xinit i3 i3blocks dkms software-properties-common curl
 add-apt-repository ppa:mmstick76/alacritty
 apt update
 
-apt install rofi lxappearance mpv fonts-firacode cmus feh vim-gtk3 htop compton ranger alacritty numlockx xbacklight gir1.2-gtop-2.0 lm-sensors network-manager bc sxiv flameshot
+apt install rofi lxappearance mpv fonts-firacode cmus feh neovim htop compton ranger alacritty numlockx xbacklight gir1.2-gtop-2.0 lm-sensors network-manager bc sxiv flameshot
 
 
