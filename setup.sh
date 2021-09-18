06edb97f38963aaf982c67d9cae786eda35fee22
John Wreck <john.wreck@gmail.com>
2021-09-18 14:30:40 +0200

    setup edit

--- setup.sh
+++ setup.sh
@@ -27,12 +27,11 @@ ln -s $HOME/.dotfiles/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.
 ln -s $HOME/.dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim && \
 ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
 ## i3-wm
-#ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc && \
+#ln -s $HOME/.dotfiles/i3/config $HOME/.config/i3/config && \
 #ln -s $HOME/.dotfiles/.i3blocks.conf $HOME/.i3blocks.conf && \
 #ln -s $HOME/.dotfiles/.scripts $HOME/.scripts && \
 #ln -s $HOME/.dotfiles/.icons $HOME/.icons && \
 #ln -s $HOME/.dotfiles/.fonts $HOME/.fonts && \
-#ln -s $HOME/.dotfiles/i3/config $HOME/.config/i3/config && \
 #ln -s $HOME/.dotfiles/dunst $HOME/.config/dunst
 ##
 if [ $? -eq 0 ]
