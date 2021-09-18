42ba4d048b21f8d18f41cbdbb58bf8b607eb62e1
John Wreck <john.wreck@gmail.com>
2021-09-18 14:25:39 +0200

    setup edit

--- i3_install.sh
+++ i3_install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 ## i3 install Ubuntu 18.04(netinstall)
 apt update
 apt install xinit i3 i3blocks dkms software-properties-common curl
@@ -6,5 +7,4 @@ add-apt-repository ppa:mmstick76/alacritty
 apt update
 
 apt install rofi lxappearance mpv fonts-firacode cmus feh neovim htop compton ranger alacritty numlockx xbacklight gir1.2-gtop-2.0 lm-sensors network-manager bc sxiv flameshot
-
 
--- setup.sh
+++ setup.sh
@@ -1,33 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#mv $HOME/.config/dunst/ $HOME/.config/dunst.bak/ 2>/dev/null
-#mv $HOME/.vimrc $HOME/.vimrc.bak 2>/dev/null
-#mv $HOME/.i3blocks.conf $HOME/.i3blocks.conf.bak 2>/dev/null
-#mv $HOME/.scripts/ $HOME/.scripts.bak/ 2>/dev/null
-mv $HOME/.bashrc $HOME/.bashrc.bak 2>/dev/null
-#mv $HOME/.icons/ $HOME/.icons.bak/ 2>/dev/null
-#mv $HOME/.fonts/ $HOME/.fonts.bak/ 2>/dev/null
-#mv $HOME/.tmux.conf $HOME/.tmux.conf.bak 2>/dev/null
-#mv $HOME/.config/i3/config $HOME/.config/i3/config.bak 2>/dev/null
-mv $HOME/.config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml.bak 2>/dev/null
-mv $HOME/.config/nvim/init.vim $HOME/.config/nvim/init.vim.bak 2>/dev/null
-#
-echo "Backup done."
-#
+mv $HOME/.bashrc $HOME/.bashrc.bak 2>/dev/null && \
+mv $HOME/.config/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml.bak 2>/dev/null && \
+mv $HOME/.config/nvim/init.vim $HOME/.config/nvim/init.vim.bak 2>/dev/null && \
+mv $HOME/.tmux.conf $HOME/.tmux.conf.bak 2>/dev/null 
+## i3-wm
+#mv $HOME/.config/i3/config $HOME/.config/i3/config.bak 2>/dev/null && \
+#mv $HOME/.config/dunst/ $HOME/.config/dunst.bak/ 2>/dev/null && \
+#mv $HOME/.scripts/ $HOME/.scripts.bak/ 2>/dev/null && \
+#mv $HOME/.i3blocks.conf $HOME/.i3blocks.conf.bak 2>/dev/null && \
+#mv $HOME/.scripts/ $HOME/.scripts.bak/ 2>/dev/null && \
+#mv $HOME/.icons/ $HOME/.icons.bak/ 2>/dev/null && \
+#mv $HOME/.fonts/ $HOME/.fonts.bak/ 2>/dev/null 
+##
+if [ $? -eq 0 ]
+then
+    echo "Backup done."
+fi
+
 mkdir $HOME/.config/alacritty
-#mkdir $HOME/.config/dunst
 mkdir $HOME/.config/nvim
-#
-#ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
-#ln -s $HOME/.dotfiles/.i3blocks.conf $HOME/.i3blocks.conf
-#ln -s $HOME/.dotfiles/.scripts $HOME/.scripts
-ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
-#ln -s $HOME/.dotfiles/.icons $HOME/.icons
-#ln -s $HOME/.dotfiles/.fonts $HOME/.fonts
-ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
-#ln -s $HOME/.dotfiles/i3/config $HOME/.config/i3/config
-#ln -s $HOME/.dotfiles/dunst $HOME/.config/dunst
-ln -s $HOME/.dotfiles/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
-ln -s $HOME/.dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+#mkdir $HOME/.config/dunst
 
-echo "Links done."
+ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc && \
+ln -s $HOME/.dotfiles/alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml && \
+ln -s $HOME/.dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim && \
+ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+## i3-wm
+#ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc && \
+#ln -s $HOME/.dotfiles/.i3blocks.conf $HOME/.i3blocks.conf && \
+#ln -s $HOME/.dotfiles/.scripts $HOME/.scripts && \
+#ln -s $HOME/.dotfiles/.icons $HOME/.icons && \
+#ln -s $HOME/.dotfiles/.fonts $HOME/.fonts && \
+#ln -s $HOME/.dotfiles/i3/config $HOME/.config/i3/config && \
+#ln -s $HOME/.dotfiles/dunst $HOME/.config/dunst
+##
+if [ $? -eq 0 ]
+then
+    echo "Links done."
+fi
