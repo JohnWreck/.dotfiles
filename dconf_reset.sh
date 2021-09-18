640e060e77835ff92d614941721c898de305d7fd
John Wreck <john.wreck@gmail.com>
2021-09-18 15:35:42 +0200

    dconf reset edit

--- dconf_reset.sh
+++ dconf_reset.sh
@@ -7,3 +7,4 @@ dconf reset /org/gnome/shell/extensions/pop-shell/focus-down
 dconf reset /org/gnome/shell/extensions/pop-shell/focus-left
 dconf reset /org/gnome/shell/extensions/pop-shell/focus-right
 dconf reset /org/gnome/shell/extensions/pop-shell/focus-up
+dconf reset /org/gnome/desktop/wm/keybindings/begin-resize
